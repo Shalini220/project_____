4952a76f2a74da4ec0686accd125024e57f1c5da
Shalini <135571841+Shalini220@users.noreply.github.com>
2024-09-04 19:17:13 +0530

    -

--- weatherApp/src/apiKeys.js
+++ weatherApp/src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{50b820b9ec7a0475f3159d15d43f4df4}",
+  key: "50b820b9ec7a0475f3159d15d43f4df4",
   base: "https://api.openweathermap.org/data/2.5/",
 };
